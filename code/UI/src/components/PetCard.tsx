919e0594684238dcb59cb76f2d65d8fa47e6ffe1
Yogesh K <yogeshcodes@zohomail.in>
2023-08-02 18:55:38 +0530

    Update color mode and remove unused css parts

--- code/UI/src/components/PetCard.tsx
+++ code/UI/src/components/PetCard.tsx
@@ -31,20 +31,17 @@ const PetCard =() => {
 
                     <Text as ='b' fontFamily={'cursive'}>Why you need Pet Insurance?</Text>
 
-                    <Text color={'gray.500'} fontSize={'sm'}>
+                    <Text color={useColorModeValue('gray.700', 'gray.400')} fontSize={'sm'}>
                         Pets are our furry, cuddly companions that bring us joy and love.
                         Owning a pet is a big responsibility, but it is also incredibly rewarding.
                     </Text>
 
-                    <Stack
-                        spacing={4}
-                        divider={<StackDivider borderColor={useColorModeValue('gray.100', 'gray.700')} />
-                        }>
-                        <UnorderedList fontSize={'sm'} fontFamily='sans-serif'  color={useColorModeValue('yellow.800','yellow.200')}>
-                            <ListItem>Protect your pet from unexpected medical expenses that can be financially devastating</ListItem>
-                            <ListItem>Pet insurance can give you peace of mind knowing that you are financially protected if your pet needs expensive medical care</ListItem>
-                            <ListItem>Avoid the difficult decision to put your pet down due to unaffordable veterinary bills</ListItem>
-                            <ListItem>Enjoy peace of mind knowing your furry friend is protected against unforeseen health issues</ListItem>
+                    <Stack>
+                        <UnorderedList  fontSize={'sm'} fontFamily='sans-serif'  color={useColorModeValue('blue.700','blue.200')}>
+                            <ListItem>Protect your pet from unexpected medical expenses that can be financially devastating.</ListItem>
+                            <ListItem>Pet insurance can give you peace of mind knowing that you are financially protected if your pet needs expensive medical care.</ListItem>
+                            <ListItem>Avoid the difficult decision to put your pet down due to unaffordable veterinary bills.</ListItem>
+                            <ListItem>Enjoy peace of mind knowing your furry friend is protected against unforeseen health issues.</ListItem>
                         </UnorderedList>
                     </Stack>
                 </Stack>
