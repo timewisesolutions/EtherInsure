6028de4b8163d38ab697f85ce2788b71ac4980dc
Yogesh K <yogeshcodes@zohomail.in>
2023-08-09 14:16:52 +0530

    Footer corrected : make it responsive

--- code/UI/src/components/Footer.tsx
+++ code/UI/src/components/Footer.tsx
@@ -4,21 +4,20 @@ const Footer = () => {
   return (
     <>
         <Box
-            position="sticky"
-            bottom="0"
             alignItems={'center'}
             flexDirection={'column'}
             borderRadius='md'
             background="#38383d"
             mx={10}
+            mb={2}
             rounded={'md'}
         >
             <Text as='h2' fontSize={18} fontWeight={'semibold'} textAlign='center' color={useColorModeValue('green.300', 'green.400')}>
             Contact Us
             </Text>
-            <Flex justifyContent={'space-between'} px={10} py={2}>
-                <Text as='b' color={'linkedin.500'}>Email: contact@contact.com</Text>
-                <Text as='b'color={'linkedin.500'}>Phone: +61 1234567</Text>
+            <Flex  flexDir={{base:'column', md:'row'}} justify={'space-between'} py={2} px={10} >
+                <Text fontSize={{ base: '10px', md: '13px', lg: '16px' }} as='b'color={'linkedin.500'}>Email: contact@contact.com</Text>
+                <Text fontSize={{ base: '10px', md: '13px', lg: '16px' }} as='b'color={'linkedin.500'}>Phone: +61 1234567</Text>
             </Flex>
         </Box>
    </>
