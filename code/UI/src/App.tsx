a7bfdeafd0540df36cf140caebdf07f538c837b7
Yogesh K <yogeshcodes@zohomail.in>
2023-08-23 12:52:09 +0530

    Move thirdweb provider to App from Main

--- code/UI/src/App.tsx
+++ code/UI/src/App.tsx
@@ -3,9 +3,16 @@ import Policy from "@/pages/Policy"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Claims from "./pages/Claims";
 import Dashboard from "./pages/Dashboard";
+import { ThirdwebProvider, useChain, useChainId } from "@thirdweb-dev/react";
+import {Sepolia, Goerli, Localhost} from '@thirdweb-dev/chains'
 
 const App = () => {
+    const activeChain = Localhost;
     return (
+        <ThirdwebProvider
+            clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
+            activeChain={activeChain}
+        >
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
@@ -16,6 +23,7 @@ const App = () => {
                 <Route path="/*" element={<Home/>}></Route>
             </Routes>
         </BrowserRouter>
+        </ThirdwebProvider>
     );
 }
 export default App;
--- code/UI/src/main.tsx
+++ code/UI/src/main.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { ThirdwebProvider, coinbaseWallet, metamaskWallet } from "@thirdweb-dev/react";
+//import { ThirdwebProvider, coinbaseWallet, metamaskWallet } from "@thirdweb-dev/react";
 import { ChakraProvider } from '@chakra-ui/react'
-import {Sepolia, Localhost} from '@thirdweb-dev/chains'
+
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = Localhost;
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
     <React.StrictMode>
-        <ThirdwebProvider
-            clientId={import.meta.env.VITE_TEMPLATE_CLIENT_ID}
-            activeChain={activeChain}
-        >
             <ChakraProvider>
                 <App />
             </ChakraProvider>
-        </ThirdwebProvider>
     </React.StrictMode>
 );
